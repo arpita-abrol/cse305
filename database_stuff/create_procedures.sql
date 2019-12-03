6a662180b4fa19a677ff0ba123770bf69d66e67d
Arpita Abrol <arpita-abrol@users.noreply.github.com>
2019-12-03 11:40:04 -0500

    salesreport

--- database_stuff/create_procedures.sql
+++ database_stuff/create_procedures.sql
@@ -148,10 +148,19 @@ CREATE PROCEDURE getSalesReport(IN targetDate DATE)
 BEGIN
     (SELECT AccountType, COUNT(*) * 10 FROM Account A1 WHERE A1.AccountType = 'Limited' AND DateOpened < targetDate )
     UNION
-    (SELECT AccountType, COUNT(*) * 20 FROM Account A1 WHERE A1.AccountType = 'Unlimited-1' AND DateOpened < targetDate )
+    (SELECT AccountType, COUNT(*) * 15 FROM Account A1 WHERE A1.AccountType = 'Unlimited-1' AND DateOpened < targetDate )
     UNION
-    (SELECT AccountType, COUNT(*) * 30 FROM Account A1 WHERE A1.AccountType = 'Unlimited-2' AND DateOpened < targetDate )
+    (SELECT AccountType, COUNT(*) * 20 FROM Account A1 WHERE A1.AccountType = 'Unlimited-2' AND DateOpened < targetDate )
     UNION
-    (SELECT AccountType, COUNT(*) * 40 FROM Account A1 WHERE A1.AccountType = 'Unlimited-3' AND DateOpened < targetDate );
+    (SELECT AccountType, COUNT(*) * 25 FROM Account A1 WHERE A1.AccountType = 'Unlimited-3' AND DateOpened < targetDate );
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE getMoviesByActor(IN actorName CHAR(20), OUT Id INTEGER, OUT MovieName CHAR(20), OUT MovieType Char(20))
+BEGIN
+	SELECT M.Id, M.MovieName, M.MovieType
+    FROM Movie M
+    WHERE M.Id == AppearedIn.MovieId;
 END$$
 DELIMITER ;
