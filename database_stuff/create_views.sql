c12c914cce43a04c1b27941a4b6631f585ce3e36
Arpita Abrol <arpita-abrol@users.noreply.github.com>
2019-12-04 09:11:44 -0500

    orderhistory

--- database_stuff/create_views.sql
+++ database_stuff/create_views.sql
@@ -12,7 +12,6 @@ AS (
     WHERE E.Id=P.SSN AND P.Zipcode=L.Zipcode
 );
 
-
 CREATE VIEW MostActiveCR (CustRepId, RentalNum, Address, LastName, FirstName, City, State, Email, Zipcode, Telephone, StartDate, HourlyRate) AS (
     SELECT  CR.CustRepId, CR.RentalNum, P.Address, P.LastName, P.FirstName, L.City, L.State, P.Email, P.Zipcode, P.Telephone, E.StartDate, E.HourlyRate
     FROM (SELECT COUNT(R.CustRepId)RentalNum, R.CustRepId
@@ -23,7 +22,6 @@ CREATE VIEW MostActiveCR (CustRepId, RentalNum, Address, LastName, FirstName, Ci
     ORDER BY(RentalNum) DESC
     LIMIT 1
 );
-
 
 CREATE VIEW MostActiveCustomers (CustomerId, RentalNum, Address, LastName, FirstName, City, State, Email, Zipcode, Telephone, CreditCardNumber, Rating) AS (
     SELECT  CR.AccountId, CR.RentalNum, P.Address, P.LastName, P.FirstName, L.City, L.State, P.Email, P.Zipcode, P.Telephone, C.CreditCardNumber, C.Rating
@@ -36,12 +34,10 @@ CREATE VIEW MostActiveCustomers (CustomerId, RentalNum, Address, LastName, First
     LIMIT 1
 );
 
-
 CREATE VIEW MailingList(CustomerId, CustomerFirstName, CustomerLastName, Email, AccountType) AS (
 	SELECT C.Id, C.FirstName, C.LastName, C.Email, A.AccountType
 	FROM Account A JOIN (SELECT C1.Id, P.FirstName, P.LastName, C1.Email FROM Customer C1 JOIN Person P ON C1.Id = P.SSN) C ON A.Customer = C.Id
 );
-
 
 CREATE VIEW viewBestSeller(MovieId, MovieName, MovieType, DistrFee, NumCopies, Rating, Views) AS (
     Select M.Id, M.MovieName, M.MovieType, M.DistrFee, M.NumCopies, M.Rating, Count(*)
@@ -50,13 +46,11 @@ CREATE VIEW viewBestSeller(MovieId, MovieName, MovieType, DistrFee, NumCopies, R
     Group by M.Id
     Order By Count(*) DESC);
     
-    
 Create VIEW GetMovieInfo(MovieName, MovieId, MovieType, Actor) AS (
     Select M.MovieName, M.Id, M.MovieType, A.ActorName
     From Actor A, AppearedIn AI, Movie M
     Where AI.ActorId=A.Id AND M.Id=AI.MovieId
 );
-
 
 CREATE VIEW CurrentMovieRentals (AccountId, CustRepId, OrderId, MovieId, MovieName, MovieType, Customer, LastName, FirstName, OrderTableDateTime, ReturnDate) 
 AS (
@@ -65,6 +59,20 @@ AS (
     WHERE R.MovieId=M.Id AND O.Id=A.Id AND R.AccountId=O.Id AND O.ReturnDate IS NULL AND A.Customer=P.SSN
     GROUP BY R.MovieId
     );
+
+CREATE VIEW viewMovieQueue (MovieName, MovieRating, MovieType, MovieId, CustomerId) AS (
+    Select M.MovieName, M.Rating, M.MovieType, M.Id, A.Customer
+    From MovieQ MQ, Movie M, Account A
+    Where A.Id = MQ.AccountId AND M.Id = MQ.MovieId );
+
+CREATE VIEW getOrderHistory(AccountId, CustomerId, MovieId, OrderId, RentDate, ReturnDate, EmployeeId) AS(
+    Select R.AccountId, A.Customer, R.MovieId, O.Id, O.OrderTableDateTime, O.ReturnDate, R.CustRepId
+    From Rental R, OrderTable O, Account A
+    Where R.AccountId=A.Id AND R.OrderId=O.Id
+);
+
+
+
 
 
 CREATE VIEW MostRented (RentalNum, MovieName) AS (
@@ -90,51 +98,6 @@ CREATE VIEW MovieSuggestion(MovieName, MovieType, Rating) AS (
         From Rental R, OrderTable O, Movie M2
         Where R.AccountId=?? AND R.OrderId=O.Id AND R.MovieId = M2.Id )
 );
-
-
-CREATE VIEW CurrentRentals (AccountId, OrderTableDateTime, MovieId, MovieName) AS (
-    SELECT Rental.AccountId, OrderTable.OrderTableDateTime, Rental.MovieId, Movie.MovieName
-    FROM (Rental JOIN OrderTable ON Rental.OrderID = OrderTable.Id) JOIN Movie ON MovieId = Movie.Id
-    WHERE OrderTable.ReturnDate IS NULL AND A.Customer=?
-    ORDER BY Rental.AccountId ASC, OrderTableDateTime ASC
-);
-
-
-CREATE VIEW viewMovieQueue (MovieName, MovieRating) AS (
-    Select MovieName,Rating
-    From MovieQ MQ, Movie M, Account A
-    Where A.Customer = ? AND A.Id = MQ.AccountId AND M.Id = MQ.MovieId );
-
-
-CREATE VIEW getOrderHistory(AccountId, MovieName, OrderId, RentDate, ReturnDate, Customer) AS(
-    Select R.AccountId, M.MovieName, O.Id, O.OrderTableDateTime, O.ReturnDate, A.Customer
-    From Rental R, OrderTable O, Movie M, Account A
-    Where R.AccountId=A.Id AND R.OrderId=O.Id AND R.MovieId = M.Id AND A.Customer= ?
-);
-
-
-CREATE VIEW SearchMovieType (MovieType, MovieName) AS (
-SELECT MovieType, MovieName
-FROM (SELECT COUNT(*) RentalNum, MovieId
-    FROM Rental R
-    GROUP BY (MovieId)) RC, Movie M
-WHERE M.Id=RC.MovieId AND M.NumCopies>RC.RentalNum AND MovieType=?
-);
-
-
-CREATE VIEW SearchByMovieKeyword(MovieName) AS 
-SELECT M.MovieName
-FROM (SELECT COUNT(*) RentalNum, MovieId
-    FROM Rental R
-    GROUP BY (MovieId)) RC, Movie M
-WHERE RC.MovieId=M.Id AND M.NumCopies>RC.RentalNum AND M.MovieName LIKE ‘%?%’;
-
-
-Create VIEW actorsIn(MovieName, Rating,Actor) AS
-    Select M.MovieName, M.Rating, A.ActorName
-    From Actor A, AppearedIn AI, Movie M
-    Where AI.ActorId=A.Id AND
-	M.Id=AI.MovieId AND A.ActorName=?;
 
 
 CREATE VIEW recommended1(MovieName, Rating) AS
